d209ea3c83c753977b694cbb72208d2c42b7e501
JhoneLeedx <lijing930615@live.com>
2017-04-26 17:55:10 +0800

    。，。撒旦撒

--- app/src/main/java/com/jhonelee/chat/ui/AddFriendActivity.java
+++ app/src/main/java/com/jhonelee/chat/ui/AddFriendActivity.java
@@ -1,8 +1,11 @@
 package com.jhonelee.chat.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -14,9 +17,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jhonelee.chat.R;
+import com.jhonelee.chat.ui.contact.ContactAdapter;
 import com.tencent.TIMAddFriendRequest;
 import com.tencent.TIMFriendResult;
 import com.tencent.TIMFriendshipManager;
+import com.tencent.TIMUserProfile;
 import com.tencent.TIMUserSearchSucc;
 import com.tencent.TIMValueCallBack;
 
@@ -25,6 +30,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by JhoneLee on 2017/4/25.
@@ -40,6 +46,8 @@ public class AddFriendActivity extends AppCompatActivity implements TextView.OnE
     RecyclerView recycler;
 
     private String TAG = "AddFriendActivity";
+    private List<TIMUserProfile> list;
+    private ContactAdapter adapter;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +57,30 @@ public class AddFriendActivity extends AppCompatActivity implements TextView.OnE
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         etSearch.setOnEditorActionListener(this);
-    }
 
+        list = new ArrayList<>();
+        adapter = new ContactAdapter(this,list);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(adapter);
+    }
+    @OnClick(R.id.ibt_search)
+    public void Click(){
+        Log.d(TAG, "search user failed, code:" +etSearch.getText().toString().trim());
+        TIMFriendshipManager.getInstance().searchUser(etSearch.getText().toString().toLowerCase().trim(),0, 10, new TIMValueCallBack<TIMUserSearchSucc>() {
+            @Override
+            public void onError(int code, String desc) {
+                Log.d(TAG, "search user failed, code:" + code + " desc:" + desc);
+            }
+
+            @Override
+            public void onSuccess(TIMUserSearchSucc timUserSearchSucc) {
+                Log.d(TAG, "search user succ. total:" + timUserSearchSucc.getTotalNum() + "|vecSize:" + timUserSearchSucc.getInfoList().size());
+                list.clear();
+                list.addAll(timUserSearchSucc.getInfoList());
+                handler.sendEmptyMessage(0);
+            }
+        });
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
@@ -65,19 +95,18 @@ public class AddFriendActivity extends AppCompatActivity implements TextView.OnE
     @Override
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 
-        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-            TIMFriendshipManager.getInstance().searchUser(etSearch.getText().toString().toLowerCase().trim(),0, 10, new TIMValueCallBack<TIMUserSearchSucc>() {
-                @Override
-                public void onError(int code, String desc) {
-                    Log.d(TAG, "search user failed, code:" + code + " desc:" + desc);
-                }
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
 
-                @Override
-                public void onSuccess(TIMUserSearchSucc timUserSearchSucc) {
-                    Log.d(TAG, "search user succ. total:" + timUserSearchSucc.getTotalNum() + "|vecSize:" + timUserSearchSucc.getInfoList().size());
-                }
-            });
         }
         return false;
     }
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what==0){
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/jhonelee/chat/ui/contact/ContactAdapter.java
@@ -0,0 +1,66 @@
+package com.jhonelee.chat.ui.contact;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.jhonelee.chat.R;
+import com.jhonelee.chat.weidget.CircleImageView;
+import com.tencent.TIMUserProfile;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by JhoneLee on 2017/4/26.
+ */
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactHolder> {
+
+
+    private Context mContext;
+    private List<TIMUserProfile> mList;
+
+    public ContactAdapter(Context mContext, List<TIMUserProfile> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+    }
+
+    @Override
+    public ContactHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_add_friend, parent, false);
+        return new ContactHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ContactHolder holder, int position) {
+        TIMUserProfile userProfile = mList.get(position);
+        Glide.with(mContext).load(userProfile.getFaceUrl()).into(holder.image);
+        holder.textSignname.setText(userProfile.getSelfSignature());
+        holder.tvNickname.setText(userProfile.getNickName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    class ContactHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.image)
+        CircleImageView image;
+        @BindView(R.id.tv_nickname)
+        TextView tvNickname;
+        @BindView(R.id.text_signname)
+        TextView textSignname;
+        public ContactHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
